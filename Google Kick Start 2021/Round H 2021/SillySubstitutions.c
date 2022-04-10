936c59a3b2355f0253a50022320a0fdc5852579c
TanJohn <lehoangtan1604@gmail.com>
2022-04-10 17:24:57 +0700

    lap theo length cua chuoi

--- Google Kick Start 2021/Round H 2021/SillySubstitutions.c
+++ Google Kick Start 2021/Round H 2021/SillySubstitutions.c
@@ -19,7 +19,6 @@ for (int i = 0; i < nosample; i++)
     scanf("%d",&n);
     p_result[i]=(char*)malloc(n*1);
     scanf("%s",*(p_result+i));
-    
     xuly(*(p_result+i),n);
   }
 
@@ -29,7 +28,10 @@ for (int i = 0; i < nosample; i++)
         printf("Case #%d: %s\n",i+1,*(p_result+i));
     }
 
-    
+    // int n=100;
+    // char a[]="4602047471535800819687071535975850388420944102148287614970644418094442274396383605310959617980638464";
+    // xuly(a,n);
+    // printf("%s",a);
     return 0;
 }
 
@@ -39,54 +41,54 @@ int i=0;
 while(i<len){
     while(p= strstr (a,"01")){//22011
         strncpy (p,"2",1);    //22211
-        i++;
+        
         strcpy ((p+1),(p+2));
         
     }
     while(p= strstr (a,"12")){
         strncpy (p,"3",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
 
     }
     while(p= strstr (a,"23")){
         strncpy (p,"4",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"34")){
         strncpy (p,"5",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"45")){
         strncpy (p,"6",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"56")){
         strncpy (p,"7",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"67")){
         strncpy (p,"8",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"78")){
         strncpy (p,"9",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"89")){
         strncpy (p,"0",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     while(p= strstr (a,"90")){
         strncpy (p,"1",1);
-        i++;
+        
         strcpy ((p+1),(p+2));
     }
     i++;
